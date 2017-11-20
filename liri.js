a721588eef1e1df8b309734b1c9b5562df62e3ab
JQuinnie <jenn8866@gmail.com>
2017-11-20 17:28:18 -0600

    added Twitter API for tweets using npm

--- liri.js
+++ liri.js
@@ -1,2 +1,19 @@
 /* At the top of the liri.js file, write the code you need to grab the data from keys.js.
 Then store the keys in a variable. */
+
+var key = require("./keys.js");
+
+var Twitter = require('twitter');
+
+var client = new Twitter(key.twitter);
+
+var params = {
+	count: 20,
+};
+client.get('statuses/user_timeline', params, function (error, tweets, response) {
+	if (!error) {
+		console.log(tweets[0].text);
+	} else{
+		console.log(error);
+	}
+});
